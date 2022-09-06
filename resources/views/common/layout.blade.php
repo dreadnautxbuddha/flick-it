39642f929460be58a9ce955a58cf83b81e836bd1
Peter Cortez <innov.petercortez@gmail.com>
2022-09-07 02:47:24 +0800

    Add margin to main

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -9,7 +9,7 @@
     @include('common.styles')
 </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="main relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <div class="container mx-auto">
                 @include('common.navigation')
 
