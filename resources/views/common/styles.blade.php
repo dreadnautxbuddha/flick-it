7dbcd99028d51b2679441339af00dd71a304393c
Peter Cortez <innov.petercortez@gmail.com>
2022-09-07 02:11:41 +0800

    Update modal design so that text is on the right

--- resources/views/common/styles.blade.php
+++ resources/views/common/styles.blade.php
@@ -3,3 +3,10 @@
 
 <!-- Styles -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+<style>
+    .card-horizontal {
+        display: flex;
+        flex: 1 1 auto;
+    }
+</style>
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,6 +3,49 @@
 @section('title', 'Gallery')
 
 @section('content')
+    <template class="modal-template">
+        <div class="flickr-${id} modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog mw-100 modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close float-right" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card">
+                            <div class="card-horizontal">
+                                <div class="img-square-wrapper">
+                                    <img class="img-responsive mx-auto" src="${src}">
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title">Card title</h4>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </template>
+    <template class="category-button-template">
+        <button
+            class="nav-link"
+            id="v-pills-${galleryId}-tab"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-${galleryId}"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-${galleryId}"
+            data-gallery-id="${galleryId}">${text}
+        </button>
+    </template>
+    <template class="nav-target-template">
+        <div class="tab-pane d-flex justify-content-between" id="v-pills-${galleryId}" role="tabpanel" data-label="${text}" aria-labelledby="v-pills-${galleryId}-tab">
+        </div>
+    </template>
+
     <div class="col-md-12">
         <div class="row text-center gallery-title"><h3></h3></div>
         <div class="d-flex align-items-start gallery">
@@ -55,7 +98,7 @@
                 });
 
               $('.tab-pane.active').append(thumbnails);
-              $('.gallery').append(originals);
+              $('#v-pills-tabContent').append(originals);
             }
           });
         });
@@ -69,17 +112,11 @@
          * @returns {*|jQuery|HTMLElement}
          */
         const createNavButton = (galleryId, text) => {
-          return $('<button />', {
-            class: 'nav-link',
-            id: `v-pills-${galleryId}-tab`,
-            type: 'button',
-            role: 'tab',
-            text,
-            'aria-controls': `v-pills-${galleryId}`,
-            'data-bs-toggle': 'pill',
-            'data-bs-target': `#v-pills-${galleryId}`,
-            'data-gallery-id': galleryId,
-          });
+          return $(
+            interpolate(
+              $('template.category-button-template').html(), { galleryId, text }
+            )
+          );
         }
 
         /**
@@ -92,13 +129,7 @@
          * @returns {*|jQuery|HTMLElement}
          */
         const createNavTarget = (galleryId, text) => {
-          return $('<div />', {
-            class: 'tab-pane d-flex justify-content-between',
-            id: `v-pills-${galleryId}`,
-            role: 'tabpanel',
-            'data-label': text,
-            'aria-labelledby': `v-pills-${galleryId}-tab`,
-          });
+          return $(interpolate($('.nav-target-template').html(), { galleryId, text }));
         }
 
         /**
@@ -129,22 +160,26 @@
          * @returns {*|jQuery|HTMLElement}
          */
         const createOriginalImageModal = (id, src, text) => {
-          const modal = $('<div />', {
-            class: `flickr-${id} modal fade`,
-            tabindex: -1,
-            role: 'dialog'
-          });
-          const dialog = $('<div />', { class: 'modal-dialog mw-100 modal-dialog-centered' });
-          const content = $('<div />', { class: 'modal-content' });
-          const img = $('<img />', { src, class: 'img-responsive mx-auto' });
-
-          content.append(img);
-          dialog.append(content);
-          modal.append(dialog);
-
-          return modal;
+          return $(interpolate($('.modal-template').html(), { id, src, text }));
         }
 
+        /**
+         * Given key-value pairs of variables, each "${variable}" will be replaced in
+         * the string.
+         *
+         * @param string
+         * @param variables
+         * @returns {*}
+         */
+        const interpolate = (string, variables) => {
+          Object.entries(variables).forEach(variable => {
+            [key, value] = variable;
+
+            string = string.replaceAll('${'+key+'}', value);
+          });
+
+          return string;
+        };
       })
     </script>
 @endsection
